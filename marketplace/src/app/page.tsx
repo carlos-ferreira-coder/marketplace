f9180f179ea768068e4305ac208ccbbb441ea068
Carlos <ferreira.carlos.bcs@gmail.com>
2025-09-26 17:12:33 -0300

    feat: pageBar end

--- marketplace/src/app/page.tsx
+++ marketplace/src/app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
           <FilterBar />
           <PageBar />
           <ProductList />
+          <PageBar />
         </main>
       </div>
     </QueryClientProvider>
